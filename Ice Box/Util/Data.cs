cee19bc0e19e0337aaefbf1dbc0830ee16d40201
Leontopodium Nivale <103514618+LeontopodiumNivale14@users.noreply.github.com>
2024-11-23 11:14:59 -0500

    Island Sanc ID's

---
+++ Ice Box/Util/Data.cs
@@ -0,0 +1,53 @@
+﻿namespace Ice_Box.Util;
+
+public class Data
+{
+    public static class Data
+    {
+        public static int PalmLeafID = 37551;
+        public static int BranchID = 37553;
+        public static int StoneID = 37554;
+        public static int ClamID = 37555;
+        public static int LaverID = 37556;
+        public static int CoralID = 37557;
+        public static int IslewortID = 37558;
+        public static int SandID = 37559;
+        public static int VineID = 37562;
+        public static int SapID = 37563;
+        public static int AppleID = 37552;
+        public static int LogID = 37560;
+        public static int PalmLogID = 37561;
+        public static int CopperID = 37564;
+        public static int LimestoneID = 37565;
+        public static int RockSaltID = 37566;
+        public static int ClayID = 37570;
+        public static int TinsandID = 37571;
+        public static int SugarcaneID = 37567;
+        public static int CottonID = 37568;
+        public static int HempID = 37569;
+        public static int IslefishID = 37575;
+        public static int SquidID = 37576;
+        public static int JellyfishID = 37577;
+        public static int Iron_OreID = 37572;
+        public static int QuartzID = 37573;
+        public static int LeucograniteID = 37574;
+        public static int Multicolored_IslebloomsID = 39228;
+        public static int ResinID = 39224;
+        public static int CoconutID = 39225;
+        public static int BeehiveID = 39226;
+        public static int WoodOpalID = 39227;
+        public static int CoalID = 39887;
+        public static int GlimshroomID = 39889;
+        public static int Effervescent_WaterID = 39892;
+        public static int ShaleID = 39888;
+        public static int MarbleID = 39890;
+        public static int Mythril_OreID = 39888;
+        public static int SpectrineID = 39893;
+        public static int Durium_SandID = 41630;
+        public static int Yellow_Copper_OreID = 41631;
+        public static int Gold_OreID = 41632;
+        public static int Hawks_Eye_SandID = 41633;
+        public static int Crystal_FormationID = 41634;
+    }
+}
+
