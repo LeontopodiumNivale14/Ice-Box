8a4e6850db5576dbf3f25102114f566d563dcaa6
UcanPatatess <ucanpatatess@gmail.com>
2024-11-24 22:37:22 +0300

    fix

--- IceBox/Service.cs
+++ IceBox/Service.cs
@@ -1,9 +1,11 @@
 using Dalamud.Plugin;
+using ECommons.Automation.NeoTaskManager;
 
 namespace IceBox
 {
     internal class Service
     {
+        public static TaskManager TaskManagerIce;
         internal static IceBox IceBox { get; set; } = null!;
         internal static Config Configuration { get; set; } = null!;
         public static IDalamudPluginInterface PluginInterface { get; set; } = null!;
--- IceBox/Windows/SettingsWindow.cs
+++ IceBox/Windows/SettingsWindow.cs
@@ -5,55 +5,40 @@ using ImGuiNET;
 
 namespace IceBox.Windows;
 
-public class ConfigWindow : Window, IDisposable
+internal class SettingsWindow : Window, IDisposable
 {
-    private Config config;
-
     // We give this window a constant ID using ###
     // This allows for labels being dynamic, like "{FPS Counter}fps###XYZ counter window",
     // and the window ID will always be "###XYZ counter window" for ImGui
-    public ConfigWindow(IceBox iceBox) : base("A Wonderful Configuration Window###With a constant ID")
+    public static new readonly string WindowName = "Config";
+    public SettingsWindow() : base(WindowName)
     {
         Flags = ImGuiWindowFlags.NoResize | ImGuiWindowFlags.NoCollapse | ImGuiWindowFlags.NoScrollbar |
                 ImGuiWindowFlags.NoScrollWithMouse;
 
         Size = new Vector2(232, 90);
         SizeCondition = ImGuiCond.Always;
-
-        config = iceBox.Config;
     }
 
     public void Dispose() { }
 
-    public override void PreDraw()
-    {
-        // Flags must be added or removed before Draw() is being called, or they won't apply
-        if (config.IsConfigWindowMovable)
-        {
-            Flags &= ~ImGuiWindowFlags.NoMove;
-        }
-        else
-        {
-            Flags |= ImGuiWindowFlags.NoMove;
-        }
-    }
 
     public override void Draw()
     {
         // can't ref a property, so use a local copy
-        var configValue = config.SomePropertyToBeSavedAndWithADefault;
+        var configValue = Configuration.SomePropertyToBeSavedAndWithADefault;
         if (ImGui.Checkbox("Random Config Bool", ref configValue))
         {
-            config.SomePropertyToBeSavedAndWithADefault = configValue;
+            Configuration.SomePropertyToBeSavedAndWithADefault = configValue;
             // can save immediately on change, if you don't want to provide a "Save and Close" button
-            config.Save();
+            Configuration.Save();
         }
 
-        var movable = config.IsConfigWindowMovable;
+        var movable = Configuration.IsConfigWindowMovable;
         if (ImGui.Checkbox("Movable Config Window", ref movable))
         {
-            config.IsConfigWindowMovable = movable;
-            config.Save();
+            Configuration.IsConfigWindowMovable = movable;
+            Configuration.Save();
         }
     }
 }
